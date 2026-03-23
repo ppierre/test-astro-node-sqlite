11901e146244550502c9f655517b8a9e165a4dac
pierre pracht <pierre.pracht@gmail.com>
2026-03-23 09:51:02 +0100

    Utilse le db exporté https://better-auth.com/docs/installation#configure-database

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,7 @@
 import { betterAuth } from "better-auth";
+import { db } from "./db";
 
 export const auth = betterAuth({
-  //...
+  // https://better-auth.com/docs/adapters/sqlite#nodejs-built-in-sqlite-experimental
+  database: db,
 });
