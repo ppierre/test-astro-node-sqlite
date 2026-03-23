09c647ec2bcb31bfb636ba1fe14e3e442a9e1056
pierre pracht <pierre.pracht@gmail.com>
2026-03-23 10:01:41 +0100

    https://better-auth.com/docs/installation#authentication-methods

--- src/auth.ts
+++ src/auth.ts
@@ -4,4 +4,7 @@ import { db } from "./db";
 export const auth = betterAuth({
   // https://better-auth.com/docs/adapters/sqlite#nodejs-built-in-sqlite-experimental
   database: db,
+  emailAndPassword: {
+    enabled: true,
+  },
 });
